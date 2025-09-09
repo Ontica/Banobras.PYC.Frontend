454ec296dd8b460988d6e1d0fdf25e34233f008c
Janeth Montero <janeth.montero@ontica.org>
2025-09-09 14:29:24 -0700

    Feat(cash-flow): add 'ConceptAnalytic' report type

--- src/app/models/cash-flow.ts
+++ src/app/models/cash-flow.ts
@@ -11,20 +11,22 @@ import { DataTable, DataTableColumn, DataTableEntry } from './_data-table';
 
 
 export enum CashFlowReportTypes {
-  CashFlow      = 'CashFlow',
-  ConceptDetail = 'ConceptDetail',
-  AccountTotals = 'AccountTotals',
-  ConceptTotals = 'ConceptTotals',
-  ProjectTotals = 'ProjectTotals'
+  CashFlow        = 'CashFlow',
+  ConceptDetail   = 'ConceptDetail',
+  AccountTotals   = 'AccountTotals',
+  ConceptTotals   = 'ConceptTotals',
+  ProjectTotals   = 'ProjectTotals',
+  ConceptAnalytic = 'ConceptAnalytic',
 }
 
 
 export const CashFlowReportTypesList: Identifiable<CashFlowReportTypes>[] = [
-  {uid: CashFlowReportTypes.CashFlow,      name: 'Flujo de efectivo'},
-  {uid: CashFlowReportTypes.ConceptDetail, name: 'Detalle a nivel de concepto'},
-  {uid: CashFlowReportTypes.AccountTotals, name: 'Totales por cuenta'},
-  {uid: CashFlowReportTypes.ConceptTotals, name: 'Totales por concepto'},
-  {uid: CashFlowReportTypes.ProjectTotals, name: 'Totales por proyecto'},
+  {uid: CashFlowReportTypes.CashFlow,        name: 'Flujo de efectivo'},
+  {uid: CashFlowReportTypes.ConceptDetail,   name: 'Detalle a nivel de concepto'},
+  {uid: CashFlowReportTypes.AccountTotals,   name: 'Totales por cuenta'},
+  {uid: CashFlowReportTypes.ConceptTotals,   name: 'Totales por concepto'},
+  {uid: CashFlowReportTypes.ProjectTotals,   name: 'Totales por proyecto'},
+  {uid: CashFlowReportTypes.ConceptAnalytic, name: 'Analítico por concepto de movimientos'},
 ];
 
 
